e4614f84180e2cd883664dbcc622ffa87c316c0b
gulyaev.eduard <gulyaev.ed.v@gmail.com>
2022-08-16 00:13:58 +0400

    ссылки и картинки

--- DAL/DbModels/Catalog.cs
+++ DAL/DbModels/Catalog.cs
@@ -10,7 +10,8 @@ namespace DAL.DbModels
 	{
 		public int Id { get; set; }
 		public string Name { get; set; }
-
+		public string Url { get; set; }
+		public string IconUrl { get; set; }
 		public List<Part> Parts { get; set; }
 	}
 }
