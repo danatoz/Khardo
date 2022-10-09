336c3199496d921d13c0feb8b40ba2d3e8536f5a
gulyaev.ed <gulyaev.ed.v@gmail.com>
2022-10-09 13:55:30 +0400

    #8 Подключить сервис rabbitmqlistener

--- UI/Services/RabbitMqListener.cs
+++ UI/Services/RabbitMqListener.cs
@@ -1,4 +1,5 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Diagnostics;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -6,6 +7,7 @@ using Microsoft.Extensions.Hosting;
 using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
 using Common.Enums;
+using Microsoft.Extensions.Logging;
 
 namespace UI.Services
 {
@@ -13,28 +15,54 @@ namespace UI.Services
 	{
 		private readonly IConnection _connection;
 		private readonly IModel _chanel;
+		private readonly ILogger<RabbitMqListener> _logger;
 
-		public RabbitMqListener()
+		public RabbitMqListener(ILogger<RabbitMqListener> logger)
 		{
 			var factory = new ConnectionFactory { HostName = "localhost" };
-			_connection = factory.CreateConnection();
-			_chanel = _connection.CreateModel();
-			_chanel.QueueDeclare(queue: nameof(QueueName.MyQueue), durable: false, exclusive: false, autoDelete: false, arguments: null);
+			try
+			{
+				_logger = logger;
+				_connection = factory.CreateConnection();
+				_chanel = _connection.CreateModel();
+				_chanel.QueueDeclare(queue: nameof(QueueName.MyQueue), durable: false, exclusive: false,
+					autoDelete: false, arguments: null);
+			}
+			catch (RabbitMQ.Client.Exceptions.BrokerUnreachableException ex)
+			{
+				_logger.LogError("Проверьте RabbitMQ, возможно не запущено приложение или проблемы с подключением");
+				_logger.LogError($"{ex.Message}");
+				_logger.LogError($"{ex.StackTrace}");
+			}
+			catch (Exception ex)
+			{
+				_logger.LogError($"{ex.Message}");
+				_logger.LogError($"{ex.StackTrace}");
+			}
 		}
 
 		protected override Task ExecuteAsync(CancellationToken stoppingToken)
 		{
-			stoppingToken.ThrowIfCancellationRequested();
-			var consumer = new EventingBasicConsumer(_chanel);
-			consumer.Received += (ch, ea) =>
+			try
 			{
-				var content = Encoding.UTF8.GetString(ea.Body.ToArray());
-				//TODO обрабатываем полученное сообщение
-				Debug.WriteLine($"Получено сообщение {content}");
-				_chanel.BasicAck(ea.DeliveryTag, false);
-			};
+				stoppingToken.ThrowIfCancellationRequested();
+				var consumer = new EventingBasicConsumer(_chanel);
+				consumer.Received += (ch, ea) =>
+				{
+					var content = Encoding.UTF8.GetString(ea.Body.ToArray());
+					//TODO обрабатываем полученное сообщение
+					Debug.WriteLine($"Получено сообщение {content}");
+					_chanel?.BasicAck(ea.DeliveryTag, false);
+				};
 
-			_chanel.BasicConsume(nameof(QueueName.MyQueue), false, consumer);
+				_chanel?.BasicConsume(nameof(QueueName.MyQueue), false, consumer);
+			}
+			catch (Exception ex)
+			{
+				_logger.LogError($"{ex.Message}");
+				_logger.LogError($"{ex.StackTrace}");
+			}
+
 
 			return Task.CompletedTask;
 		}
