4ce11819a802fe201537dac40ebdec6353d773c4
gulyaev.ed <gulyaev.ed.v@gmail.com>
2022-10-19 13:58:11 +0400

    добавил шаблоны страницы товара

--- UI/wwwroot/js/site.js
+++ UI/wwwroot/js/site.js
@@ -1,4 +1,43 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿
+// init Bloodhound with Remote + Prefetch
+var engine = new Bloodhound({
+    datumTokenizer: Bloodhound.tokenizers.obj.whitespace('name'),
+    queryTokenizer: Bloodhound.tokenizers.whitespace,
+    prefetch: {
+        url: window.location.href + 'Home/TypeaheadPrefetch/',
+        //cache: true,
+        transform: function (data) {
+            var newData = [];
+            data.forEach(function (item) {
+                newData.push({ 'name': item });
+            });
+            return newData;
+        }
+    },
+    remote: {
+        url: window.location.href + 'Home/TypeaheadQuery/' + '?query=%QUERY',
+        wildcard: '%QUERY'
+    },
+});
 
-// Write your JavaScript code.
+// init Typeahead
+$('#search_product-text').typeahead(
+    {
+        minLength: 3,
+        highlight: true
+    },
+    {
+        name: 'products',
+        source: engine,
+        display: function (item) {
+            console.log(item);
+            return `${item.vendorCode} ${item.name}`;
+        },
+        limit: 5,
+        templates: {
+            suggestion: function (item) {
+                console.log(item);
+                return '<div class="p-3 bg-light hover">' + item.vendorCode + ' ' + item.name + '</div>';
+            }
+        }
+    });
